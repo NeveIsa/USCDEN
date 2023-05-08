a25935cb931c315fe5f9d45bd539f3e9a3efddd7
sampad <sbmohant@usc.edu>
2023-05-07 20:16:02 -0700

    ok

---
+++ .ipynb_checkpoints/fb-checkpoint.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data = pd.read_csv("feedback.csv")
+
+def getfeedback(student):
+    fb = data[data["Student Name"]==student].iloc[0]["Feedback"] 
+    return fb
+     
+if __name__=="__main__":
+    fb = getfeedback("Agarwal, Shrey")
+    print(fb)
